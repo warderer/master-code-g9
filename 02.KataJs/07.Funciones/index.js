35860a78eb7871495557ad25a9718456d97e66b8
Jesua Luján <code.masterqro@gmail.com>
2021-07-07 17:31:42 -0500

    mensaje en el navegador para saber que todo está bien

--- 02.KataJs/07.Funciones/index.js
+++ 02.KataJs/07.Funciones/index.js
@@ -1,13 +1,2 @@
 document.write("TODO VA BIEN")
 
-Add()
-function Add() {
-    var myNumber = 4; 
-    console.log(myNumber + myNumber)
-}
-
-var myNumber = 4;
-function add() {
-    console.log(myNumber + myNumber);
-}
-add();
