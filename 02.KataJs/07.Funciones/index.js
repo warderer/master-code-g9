38c4ea76b46e4a77e602a418d49af2545ff588cb
Jesua Luján <code.masterqro@gmail.com>
2021-07-07 17:30:55 -0500

    FUNCIONES

---
+++ 02.KataJs/07.Funciones/index.js
@@ -0,0 +1,13 @@
+document.write("TODO VA BIEN")
+
+Add()
+function Add() {
+    var myNumber = 4; 
+    console.log(myNumber + myNumber)
+}
+
+var myNumber = 4;
+function add() {
+    console.log(myNumber + myNumber);
+}
+add();
