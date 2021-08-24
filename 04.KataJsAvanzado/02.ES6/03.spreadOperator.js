97c74652a36c010a4f3d590e4585e85bd9ac9eed
César Guerra <warderer@gmail.com>
2021-08-23 21:19:28 -0500

    spreadoperator es6

---
+++ 04.KataJsAvanzado/02.ES6/03.spreadOperator.js
@@ -0,0 +1,37 @@
+// SPREAD OPERATOR CON ARREGLOS
+// EL SPREAD OPERATOR SON LOS TRES PUNTOS ...
+
+// Fusiono el contenido de arreglos
+const misColores = ['Amarillo', 'Rojo', 'Azul'];
+const misColoresDos = ['Negro', 'Blanco', ...misColores];
+
+console.log(misColoresDos);
+
+// Otra forma de fusionar arreglos
+const misColoresTres = ['Amarillo', 'Rojo', 'Azul','Verde'];
+const misColoresCuatro = ['Negro', 'Blanco','Gris'];
+const misColoresCinco = [...misColoresCuatro, ...misColoresTres];
+console.log("Colores 5", misColoresCinco);
+
+
+/* SPREAD OPERATOR CON OBJETOS */
+const myPokemon = {
+    id:4,
+    name: 'Charmander',
+    type: 'Fuego'
+};
+
+const myComplemento = {
+    attack: 'Mordisco',
+    type: 'Hielo'
+}
+
+const myNewPokemon = {
+    ...myPokemon,
+    ...myComplemento
+}
+// Pasaron 2 cosas:
+// 1) El attack no existia en myPokemon, pero si en myComplemento, por lo que se añadio.
+// 2) Como type, ya existia, el valor de myNewPokemon 'plancha' el valor de myPokemon.
+
+console.log(myNewPokemon);
