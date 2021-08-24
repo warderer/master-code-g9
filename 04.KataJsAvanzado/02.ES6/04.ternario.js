c6c25c8ccd003ab23544bebfdc8d5323fd596c92
César Guerra <warderer@gmail.com>
2021-08-23 21:30:36 -0500

    operador ternario ES6

---
+++ 04.KataJsAvanzado/02.ES6/04.ternario.js
@@ -0,0 +1,16 @@
+/* OPERADOR TERNARIO */
+// Es una Condición IF/ELSE de una sola linea.
+// SINTAXIS: (condición) ? CodigoSiEsCierto : CodigoSiEsFalso;
+// Esto es Sugar Syntax
+
+const edad = 21;
+//Como lo haciamos normalmente:
+if (edad >= 18) {
+    console.log("Eres Mayor de Edad")
+} else {
+    console.log("Eres Menor de Edad")
+}
+//En una sola linea, hacemos la logica del IF/ELSE
+
+const mayorDeEdad = (edad >= 18) ? "Es Mayor de Edad" : "Eres Menor de Edad";
+console.log(mayorDeEdad);
