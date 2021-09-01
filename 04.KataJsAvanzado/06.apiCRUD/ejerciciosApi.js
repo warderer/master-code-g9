86534aaf4ec440f5f0d65c28068d44c56a85bf21
César Guerra <warderer@gmail.com>
2021-09-01 10:26:58 -0500

    ejercicios en diferentes APIs

---
+++ 04.KataJsAvanzado/06.apiCRUD/ejerciciosApi.js
@@ -0,0 +1,10 @@
+// 1.- Hacer una función que haga una petición     (Ejemplo: peticionLibro("i robot");
+//     Buscar el primer libro e imprimir la lista de author_name   (FOR) y el title_suggest
+//     http://openlibrary.org/search.json?q=i+robot)
+
+// 2.- Hacer una petición por author y devolver la lista de
+//     sus libros
+//     http://openlibrary.org/search.json?author=asimov
+
+// 3.- Hacer una peticion y devolver el género de la banda deseada
+//     http://www.theaudiodb.com/api/v1/json/1/search.php?s=muse
