15c6662dfc1b2f82ed8f39b7ca91e486ee8f4f8c
César Guerra <warderer@gmail.com>
2021-09-21 20:22:06 -0500

    fix popper de bootstrap

---
+++ 05.React/02.webpack-pokedex/src/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import 'bootstrap';
+import Pokedex from './Pokedex';
+
+function init(){ //App Start
+    //Solicitar los datos de los Pokémons
+    const pokemonCount = 10;
+    const URI = `https://pokeapi.co/api/v2/pokemon?limit=${pokemonCount}&offset=0'`;
+    axios.get(URI)
+        .then((response) => {
+            console.log("Status Code:", response.status);
+            //La data esta en: response.data.results
+            console.log("La Data es:", response.data.results);
+        }).catch( (error) => {
+            console.log("Error:",error);
+        })
+}
+
+init();
---
+++ 05.React/02.webpack-pokedex/src/pokedex.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+class Pokedex {
+
+}
+
+export default Pokedex;
