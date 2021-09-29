fe6010f7bcb1acb0215707f3671a83107d1f61a9
Jesua Luján <code.masterqro@gmail.com>
2021-09-28 20:13:03 -0500

    Modificamos e importamos en App nuetros componentes

---
+++ 05.React/05.react-giphy/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchBar from './Components/SearchBar';
+import ImageCard from './Components/ImageCard';
+
+import './App.css';
+
+
+class App extends React.Component{
+
+
+
+
+}
+
+
+export default App;
