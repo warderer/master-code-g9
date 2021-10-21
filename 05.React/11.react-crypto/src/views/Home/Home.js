a6f24a51227966f4d4f905555a6ec58dbeb267f8
César Guerra <warderer@gmail.com>
2021-10-20 20:36:37 -0500

    componente coin

--- 05.React/11.react-crypto/src/views/Home/Home.js
+++ 05.React/11.react-crypto/src/views/Home/Home.js
@@ -3,6 +3,7 @@ import useAxios from '../../hooks/useAxios';
 
 const Home = () => {
     const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState('');
 
     const { response, loading, error } = useAxios({
         method: 'GET',
@@ -10,41 +11,45 @@ const Home = () => {
     });
 
     const handleChange = e => {
-        return '';
+        setSearch(e.target.value)
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         if (response !== null) {
             setCoins(response);
             console.log(response);
         }
-    },[response]);
+    }, [response]);
+
+    const filteredCoins = coins && coins.filter(coin =>
+        coin.name.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
         <>
             <h1>Home</h1>
             <div className='coin-app'>
-            <div className='coin-search'>
-                <h1 className='coin-text'>Search a currency</h1>
-                <form>
-                    <input
-                        className='coin-input'
-                        type='text'
-                        onChange={handleChange}
-                        placeholder='Search'
-                    />
-                </form>
-            </div>
-            {error && (
-                <div>
-                    <p>{error.message}</p>
+                <div className='coin-search'>
+                    <h1 className='coin-text'>Search a currency</h1>
+                    <form>
+                        <input
+                            className='coin-input'
+                            type='text'
+                            onChange={handleChange}
+                            placeholder='Search'
+                        />
+                    </form>
                 </div>
-            )}
+                {error && (
+                    <div>
+                        <p>{error.message}</p>
+                    </div>
+                )}
 
-            {coins && (<div>
-                <p>Algo</p>
-            </div>)
-            }
+                {coins && (<div>
+                    <p>Algo</p>
+                </div>)
+                }
             </div>
         </>
     )
