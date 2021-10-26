62c88bba441b4f153c65739593c7532f955732f5
Jesua Luján <code.masterqro@gmail.com>
2021-10-26 12:40:10 -0500

    Solo hacemos la parte de la lógica y la llamada a la API

---
+++ 05.React/12.hoc/src/WithCurrentUser.js
@@ -0,0 +1,25 @@
+//EN NUESTRO COMPONENTE WithCurrentUser salamente hacemos la lógica
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+
+const WithCurrentUser = ComponentUser => {
+    const NewComponent = props =>{
+        const [user,setUser] = useState(null);
+
+    //con useEffect podemos inicializar variables o hacer llamadas a las APIs
+    useEffect(() =>{
+        axios.get("https://jsonplaceholder.typicode.com/users/1")
+        .then(resp=>{
+            setUser(resp.data)
+        })
+
+    }, [])//escuchamos las dependencias para saber si hay cambios
+
+
+        return <ComponentUser {...props}  user={user}/> //pasamos los props de nuestro usuario
+    }
+   
+    return NewComponent
+}
+
+export default WithCurrentUser
