cb951c6350b1ca9e51cb247aa9d07339676b7fde
Jesua Luján <code.masterqro@gmail.com>
2021-10-26 12:58:30 -0500

    Creamos otro HOC para indicarle el ID de los usuarios

--- 05.React/12.hoc/src/App.js
+++ 05.React/12.hoc/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import User from "./User";
-import WithCurrentUser from "./WithCurrentUser";
+// import WithCurrentUser from "./WithCurrentUser";
+import WithUserId from "./WithUserId";
+import './App.css'
 
 //? AQUÍ ESTAMOS CREANDO NUESTRO HIGHER ORDER COMPONENT (HOC)
-const CurrentUser = WithCurrentUser (User);
+// const CurrentUser = WithCurrentUser (User);
+const SpecificUser = WithUserId (User); 
 
 function App() {
   return (
     <>
-      <CurrentUser />
+      {/* <CurrentUser /> */}
+    <SpecificUser userId={1} />
+    <SpecificUser userId={2} />
+    <SpecificUser userId={3} />
+    <SpecificUser userId={9} />
+
     </>
   );
 }
