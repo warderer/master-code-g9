e084186c364e8a18f7b9ecbf3008c8c8a595d046
César Guerra <warderer@gmail.com>
2021-10-26 19:48:19 -0500

    Home listo y data

---
+++ 05.React/14.songlist-context/src/views/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="izquierdo">
+                <h2>Lado Izquierdo</h2>
+            </div>
+
+            <div className="derecho">
+                <h2>Derecho</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
