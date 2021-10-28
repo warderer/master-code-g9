7ee1325fd82c020425032c69c3f3e3dfd4c17819
Jesua Luján <code.masterqro@gmail.com>
2021-10-27 22:24:53 -0500

    Cargamos nuestras vistas en el index de Routes

---
+++ 05.React/13.authentication/src/Routes/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
+import Home from '../Views/Home';
+import Signup from '../Views/Signup';
+import Login from '../Views/Login';
+import Item from '../Views/Item';
+
+// DEVUELVE UN JSX PARA QUE SEA UN COMPONENTE DE REACT
+    const Logout = () => {
+       window.localStorage.removeItem('') 
+       return <Redirect to="/" />
+    }
+ 
+function index() {
+    return (
+        <Router>
+         <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/signup" component={Signup}></Route>
+                <Route exact path="/login" component={Login}></Route>
+                <Route exact path="/item" component={Item}></Route>
+                <Route exact path="/logout" component={Logout}></Route>
+          </Switch>
+        </Router>
+    )
+}
+
+export default index
