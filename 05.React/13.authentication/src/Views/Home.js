ecf6dc88a72f7d6ad55e92fe356dd0418254e400
Jesua Luján <code.masterqro@gmail.com>
2021-10-27 22:01:15 -0500

    Únicamente trabajamos la vista de Home

---
+++ 05.React/13.authentication/src/Views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+function Home() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Bienvenido a tu tiendita E-commerce DEVF!</h1>
+        </div>
+    )
+}
+
+export default Home
