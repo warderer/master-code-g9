88ce1cc6f1252211ebfc6eb6be3b9f0b4d917a81
Jesua Luján <code.masterqro@gmail.com>
2021-10-28 20:15:59 -0500

    Enviamos la data de nuestro formulario de Login

--- 05.React/13.authentication/src/Views/Login.js
+++ 05.React/13.authentication/src/Views/Login.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
-
+import axios from 'axios'
+import useForm from '../Hook/useForm'
+import { useHistory } from 'react-router'
 
 function Login() {
+
+    let history = useHistory();
+    const sendData = (data) => {
+        axios.post("https://ecomerce-master.herokuapp.com/api/v1/login", data)
+        .then((response) => {
+            console.log(response.data);
+            window.localStorage.setItem("token", response.data.token )
+            history.push('/');
+        })
+        .catch((error)=>{
+            alert(error.response.data.message)
+        })
+    }
+
+    const {input, handleInputChange, handleSubmit} = useForm (sendData,{})
+
     return (
         <div>
             <Navbar />
-            <form onSubmit="">
+            <form onSubmit={handleSubmit}>
                 <div className="cointainer">
                     <div className="row justifiy-content-center">
 
@@ -16,8 +34,8 @@ function Login() {
                                 <input type="email"
                                     name="email"
                                     className="form-control"
-                                    value=""                              
-                                    onChange=""
+                                    value={input.email}                             
+                                    onChange={handleInputChange}
                                 />
                             </div>
                         </div>
@@ -28,8 +46,8 @@ function Login() {
                                 <input type="password"
                                     name="password"
                                     className="form-control"
-                                    value=""
-                                    onChange=""
+                                    value={input.password}  
+                                    onChange={handleInputChange}
                                 />
                             </div>
                         </div>
