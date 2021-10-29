942d161481bbe6a0aff4f758c621359050942427
Jesua Luján <code.masterqro@gmail.com>
2021-10-28 20:17:24 -0500

    Validamos si el usuario inicio sesión

--- 05.React/13.authentication/src/Components/Navbar/Navbar.js
+++ 05.React/13.authentication/src/Components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import payload from '../../Utils/payload'
 
 function Navbar() {
+    const user = payload();
     return (
-        
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark"> 
         <Link className="navbar-brand" to="/" >
             Item App
@@ -18,24 +19,43 @@ function Navbar() {
         aria-label="Toggle navigation"
         >
         <span className="navbar-toggle-icon">
-
         </span>
         </button>
         <div  className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-            <li className="nav-item active">
-            <Link className="nav-link" to="/login" >
-             Login
-            </Link>
-            </li>
-            <li className="nav-item">
-            <Link className="nav-link" to="/signup" >
-             Signup
-            </Link>
-            </li>
-        </ul>
-        </div>
-        </nav>
+            { user ? (
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/profile">
+                        HOLA, BIENVENIDO! 
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/item">
+                         Mi item
+                        </Link> 
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/logout">
+                        Logout
+                        </Link> 
+                    </li>
+                </ul>
+             ) : ( <ul className="navbar-nav">
+             <li className="nav-item active">
+             <Link className="nav-link" to="/login" >
+              Login
+             </Link>
+             </li>
+             <li className="nav-item">
+             <Link className="nav-link" to="/signup" >
+              Signup
+             </Link>
+             </li>
+         </ul>
+       
+             )}
+          </div>
+         </nav>
     )
 }
 
