36ff04f7b9290f55052b61eeae87a63478c7f85f
Jesua Luján <code.masterqro@gmail.com>
2021-11-02 20:55:20 -0600

    Trabajamos nuestro jsx y le pasamos los props

--- 05.React/15.bootstrap-sass/src/Components/Card.js
+++ 05.React/15.bootstrap-sass/src/Components/Card.js
@@ -1,17 +1,23 @@
 //* ES UN COMPONENTE QUE VAMOS A REUTILIZAR
 //* CARD ES UNA TARJETA
-import React from 'react'
-import image1 from '../Assets/image1.jpg'
+import React from 'react';
+// import image1 from '../Assets/image1.jpg'
+import './cards.scss';
 
-function Card({title}) {
+function Card({title, imageSource, url}) {
     return (
-        <div className="card">
-            <img src={image1} alt="" />
-            <div className="card-body">
+        <div className="card text-center bg-dark">
+            <div className="overflow">
+            <img src={imageSource} alt="" className="card-img-top"/>
+            </div>
+            <div className="card-body text-light">
                 <h4 className="card-title">{title}</h4>
                 <p className="card-text text-secondary ">Lorem ipsum dolor sit amet consectetur 
                 adipisicing elit. Provident, deleniti.
                 </p>
+                <a href={url} className="btn btn-outline-secondary rounded-0" target="_blank">
+                    Ir a la nota 
+                </a>
             </div>
         </div>
     )
