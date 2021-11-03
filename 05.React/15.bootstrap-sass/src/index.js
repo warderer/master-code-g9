92ca713c7fb2547ddca1e5e752b65a7e0a047580
Jesua Luján <code.masterqro@gmail.com>
2021-11-02 20:06:06 -0600

    en index importamos los estilos por default de bootstrap

--- 05.React/15.bootstrap-sass/src/index.js
+++ 05.React/15.bootstrap-sass/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './Scss/custom.scss';
 
 ReactDOM.render(
   <React.StrictMode>
