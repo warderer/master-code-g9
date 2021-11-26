7da12f1a06aff064142317535266757e0bc45e96
Jesua Luján <code.masterqro@gmail.com>
2021-11-25 20:53:52 -0600

    aplicamos la técnica llamada fake it until you make it al test

--- 06.Patterns/test-driven-development/src/__tests__/main-page.test.js
+++ 06.Patterns/test-driven-development/src/__tests__/main-page.test.js
@@ -8,11 +8,13 @@ import { MainPage } from "../Components/main-page";
 
 // EL REFACTOR PUEDE SER APLICADO TANTO EN EL CÓDIGO DEL TEST, ASÍ COMO EN EL CÓDIGO
 // DE LA IMPLEMENTACIÓN
-describe ('Main Page mount', () => {
+describe ('Quotes List', () => {
     //nos devuelve un callback
-    it("must display the main page title", () => {
+    it("must display 3 quotes", async () => {
     render(<MainPage />);//hacemos un render del componente
-    expect(screen.getByRole("heading", {name: /simpsons quotes/i })).toBeInTheDocument(); 
+    expect(await screen.findAllByRole("listitem")).toHaveLength(3); 
+    
+    
     //el matcher toBeInTheDocument() no existe en el core de Jest, pero es accesible  a lo que tenemos
     //en src/setupTest.js
     })
