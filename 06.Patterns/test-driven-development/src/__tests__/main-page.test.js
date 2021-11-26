529bfa67604ed4f65235684ce40a4395c20a1574
Jesua Luján <code.masterqro@gmail.com>
2021-11-25 20:36:46 -0600

    aplicamos el refactor al test

--- 06.Patterns/test-driven-development/src/__tests__/main-page.test.js
+++ 06.Patterns/test-driven-development/src/__tests__/main-page.test.js
@@ -6,12 +6,13 @@ import { render, screen } from '@testing-library/react';
 //VAMOS A IMPORTAR UN COMPONENTE, AÚN NO LO HEMOS CREADO
 import { MainPage } from "../Components/main-page";
 
-
+// EL REFACTOR PUEDE SER APLICADO TANTO EN EL CÓDIGO DEL TEST, ASÍ COMO EN EL CÓDIGO
+// DE LA IMPLEMENTACIÓN
 describe ('Main Page mount', () => {
     //nos devuelve un callback
     it("must display the main page title", () => {
     render(<MainPage />);//hacemos un render del componente
-    expect(screen.getByText(/simpsons quotes/i)).toBeInTheDocument(); 
+    expect(screen.getByRole("heading", {name: /simpsons quotes/i })).toBeInTheDocument(); 
     //el matcher toBeInTheDocument() no existe en el core de Jest, pero es accesible  a lo que tenemos
     //en src/setupTest.js
     })
